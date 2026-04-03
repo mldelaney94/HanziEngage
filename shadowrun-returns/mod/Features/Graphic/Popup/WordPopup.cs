49ddad42012e6f951b9c0ba83d2deefa63283ee1
Matthew Delaney <mldelaney94@gmail.com>
2026-04-03 17:44:58 +1100

    top left aligned

--- shadowrun-returns/mod/Features/Graphic/Popup/WordPopup.cs
+++ shadowrun-returns/mod/Features/Graphic/Popup/WordPopup.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace ShadowrunReturnsLanguageEngage
@@ -6,16 +7,27 @@ namespace ShadowrunReturnsLanguageEngage
   {
     private static GameObject panel;
     private static UILabel label;
+    private const int PanelWidth = 300;
+    private const int PanelHeight = 400;
+    private const int TextLineWidth = 280;
 
     public static void Show(string text, UIPanel parentPanel, Vector3 worldPos)
     {
+      if (Regex.Match(text, "[0-9a-zA-Z]+").Success)
+      {
+        return;
+      }
+
       if (panel == null) Create(parentPanel);
 
-      label.text = text;
+      label.text = GetDictionaryDefinition(text);
 
       var root = NGUITools.FindInParents<UIRoot>(parentPanel.gameObject);
       var isRight = root.transform.InverseTransformPoint(worldPos).x > 0;
-      panel.transform.localPosition = isRight ? new Vector3(100, 0, 0) : new Vector3(-100, 0, 0);
+
+      panel.transform.localPosition = isRight
+        ? new Vector3(parentPanel.transform.localPosition.x + 100, 100, 0)
+        : new Vector3(parentPanel.transform.localPosition.x - 250, 100, 0);
       panel.SetActive(true);
     }
 
@@ -29,32 +41,57 @@ namespace ShadowrunReturnsLanguageEngage
       var root = NGUITools.FindInParents<UIRoot>(parentPanel.gameObject);
       panel = NGUITools.AddChild<UIPanel>(root.gameObject).gameObject;
 
-      var texture = NGUITools.AddWidget<UITexture>(panel.gameObject);
-      texture = SetTexture(texture);
+      var bgTexture = NGUITools.AddWidget<UITexture>(panel.gameObject);
+      SetBackgroundTexture(bgTexture);
+      var borderTexture = NGUITools.AddWidget<UITexture>(panel.gameObject);
+      SetBorderTexture(borderTexture, bgTexture);
 
       label = NGUITools.AddWidget<UILabel>(panel.gameObject);
+      label.font = FindFont();
+      label.lineWidth = TextLineWidth;
+      label.pivot = UIWidget.Pivot.TopLeft;
+      label.transform.localPosition = new Vector3(
+        -TextLineWidth / 2f,
+        new Vector4(0, 0, PanelWidth, PanelHeight).w / 2f,
+        -1f);
 
-      foreach (var key in Globals.LabelRegistry.Keys)
-      {
-        if (key.transform != null)
-        {
-          label.font = key.font;
-          break;
-        }
-      }
 
       panel.name = "SLRETextPopup";
     }
 
-    private static UITexture SetTexture(UITexture texture)
+    private static void SetBackgroundTexture(UITexture texture)
     {
-      texture.alpha = 1;
       texture.color = NGUITools.ParseColor("060606", 0);
-      texture.transform.localScale = new Vector3(300, 400, 1f);
-      texture.material = new Material(Shader.Find("Unlit/Transparent Colored"));
-      texture.depth = 0;
+      texture.transform.localScale = new Vector3(PanelWidth, PanelHeight, 1f);
+      texture.material = new Material(Shader.Find("Unlit/Transparent Colored"))
+      {
+        renderQueue = 1
+      };
+    }
+    private static void SetBorderTexture(UITexture borderTexture, UITexture textureToBorder)
+    {
+      borderTexture.color = NGUITools.ParseColor("62b6bd", 0);
+      borderTexture.transform.localScale = new Vector3(PanelWidth + 1, PanelHeight + 1, 1f);
+      borderTexture.material = new Material(Shader.Find("Unlit/Transparent Colored"))
+      {
+        renderQueue = textureToBorder.material.renderQueue - 1
+      };
+    }
 
-      return texture;
+    private static UIFont FindFont()
+    {
+      foreach (var key in Globals.LabelRegistry.Keys)
+      {
+        if (key.transform != null) return key.font;
+      }
+      return null;
+    }
+
+    private static string GetDictionaryDefinition(string word)
+    {
+      return "{{EFD27B}}" + word + "{{-}}" + '\n'
+        + Globals.CEDict[word]["pinyin"] + '\n'
+        + Globals.CEDict[word]["english"];
     }
   }
 }
